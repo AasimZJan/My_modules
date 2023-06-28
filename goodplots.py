abae0a2038be6e39c38f0f43f251764ea38a8ba7
Aasim Z Jan <58118417+AasimZahoor@users.noreply.github.com>
2023-06-28 13:10:45 +0530

    second commit

---
+++ goodplots.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 08:40:03 2022
+
+@author: aasim
+"""
+
+import matplotlib.pyplot as plt
+plt.rcParams.update({
+'axes.labelsize': 20,
+'axes.titlesize': 20,
+'font.size': 20,
+'legend.fontsize': 13,
+'xtick.labelsize': 10,
+'figure.dpi':400,
+'ytick.labelsize': 10,
+'font.family': 'serif',
+'font.sans-serif': ['Bitstream Vera Sans'],
+'font.serif': ['Times New Roman'],
+'text.usetex':True
+}
+)
